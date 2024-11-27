7cbbcf009f992f14f29ecd7e9d9f2b2e276c213e
sof4k1n <qgor31@mail.ru>
2024-11-28 02:22:23 +0500

    + Выездное меню (Ссылки в нем пока без ховеров)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './styles/main.scss';
-import logo from './images/logo.svg';
+
+const menu_btn = document.querySelector('#menu-open');
+const menu = document.querySelector('#menu');
+const menu_exit = document.querySelector('#menu-exit');
+
+menu_btn.addEventListener('click', () => {
+    menu.classList.add('open');
+});
+
+menu_exit.addEventListener('click', () => {
+    menu.classList.remove('open');
+});
